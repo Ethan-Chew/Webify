7b944fbfd2c60bf4422b4cd7f08e991774bebc8a
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-11-12 11:53:24 +0800

    feat: working carousel

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(toTop)
     let hasChanged = false
 
+    // Handle Page Scroll
     window.addEventListener('scroll', function() {
         let scrollDist = document.documentElement.scrollTop || document.body.scrollTop || 0
     
@@ -16,4 +17,41 @@ document.addEventListener("DOMContentLoaded", function() {
             hasChanged = false
         }
     })
+
+    // Testimonial Carousel
+    let testimonialIndex = 0
+    const carousel = document.getElementsByClassName("carousel-page")
+    const pagination = document.getElementsByClassName("pagination-circle")
+    const chevrons = document.getElementsByClassName("carousel-icon")
+    for (let i = 0; i < chevrons.length; i++) {
+        // console.log(chevrons[i])
+        chevrons[i].addEventListener("click", function () {
+            const direction = chevrons[i].className.baseVal.includes("left") ? "left" : "right"
+
+            // Hide the current carousel slider, update pagination
+            carousel[testimonialIndex].setAttribute("id", "hidden")
+            pagination[testimonialIndex].setAttribute("id", "")
+
+            // Update Index
+            if (direction === "left") {
+                // Decrease the testimonialIndex
+                if (testimonialIndex - 1 < 0) {
+                    testimonialIndex = carousel.length -1
+                } else {
+                    testimonialIndex -= 1
+                }
+            } else {
+                // Increase the testimonialIndex
+                if (testimonialIndex + 1 >= carousel.length) {
+                    testimonialIndex = 0
+                } else {
+                    testimonialIndex += 1
+                }
+            }
+
+            // Show the new carousel slider, update pagination
+            carousel[testimonialIndex].setAttribute("id", "")
+            pagination[testimonialIndex].setAttribute("id", "active")
+        })
+    }
 })
